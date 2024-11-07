63590a1461451a1563a97547a00e7bd615c518dd
Gustavo Souto <gusdev.pro@gmail.com>
2024-11-07 18:04:54 -0300

    Aula de Herança

---
+++ aula-pilares-heranca/src/ComputadorPedrinho.java
@@ -0,0 +1,18 @@
+
+public class ComputadorPedrinho {
+    public static void main(String[] args) {
+        
+        MSNMessenger msn = new MSNMessenger();
+        msn.enviarMensagem();
+        msn.receberMensagem();
+
+        FacebookMessenger facebook = new FacebookMessenger();
+        facebook.enviarMensagem();
+        facebook.receberMensagem();
+
+        Telegram telegram = new Telegram();
+        telegram.enviarMensagem();
+        telegram.receberMensagem();
+
+    }
+}
