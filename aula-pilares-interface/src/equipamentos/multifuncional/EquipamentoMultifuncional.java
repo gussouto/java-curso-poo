fcbe7ea42886246e3d5d9a183763131314c4fb84
Gustavo Souto <gusdev.pro@gmail.com>
2024-11-07 20:39:29 -0300

    Aula de Interface

---
+++ aula-pilares-interface/src/equipamentos/multifuncional/EquipamentoMultifuncional.java
@@ -0,0 +1,26 @@
+package equipamentos.multifuncional;
+
+import equipamentos.copiadora.Copiadora;
+import equipamentos.digitalizadora.Digitalizadora;
+import equipamentos.impressora.Impressora;
+
+public class EquipamentoMultifuncional implements Copiadora, Digitalizadora, Impressora {
+
+    @Override
+    public void copiar() {
+        System.out.println("Copiando via equipamento MultiFuncional");
+    }
+
+    @Override
+    public void imprimir() {
+        System.out.println("Imprimindo via equipamento MultiFuncional");
+    }
+
+    @Override
+    public void digitalizar() {
+        System.out.println("Digitalizando via equipamento MultiFuncional");
+    }
+    
+
+
+}
---
+++ aula-pilares-interface/src/estabelecimento/Fabrica.java
@@ -0,0 +1,31 @@
+package estabelecimento;
+
+import equipamentos.copiadora.Copiadora;
+import equipamentos.digitalizadora.Digitalizadora;
+import equipamentos.impressora.Deskjet;
+import equipamentos.impressora.Impressora;
+import equipamentos.multifuncional.EquipamentoMultifuncional;
+
+public class Fabrica {
+    
+    public static void main(String[] args) {
+
+        EquipamentoMultifuncional em = new EquipamentoMultifuncional();
+
+        Deskjet deskjet = new Deskjet();
+
+        Impressora impressora = deskjet;
+        Digitalizadora digitalizadora = em;
+        Copiadora copiadora = em;
+        
+
+        impressora.imprimir();
+        digitalizadora.digitalizar();
+        copiadora.copiar();
+
+        
+
+
+    }
+
+}
